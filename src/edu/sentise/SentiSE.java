900c34cc6371aeaeb0194675a856d21ff6d6ce6a
tanzeerH <hossaintanzeer@gmail.com>
2017-12-27 14:04:12 -0500

    contraction applied to sentiment texts

--- src/edu/sentise/SentiSE.java
+++ src/edu/sentise/SentiSE.java
@@ -13,17 +13,19 @@ import edu.sentise.util.Util;
 public class SentiSE {
 
 	private static ArrayList<SentimentData> sentimentDataList=new ArrayList<>();
-	private static HashMap<String, String> contractionMap=new HashMap<>();
+	
 	public static void main(String[] args) {
 		
 		BufferedReader bufferedReader=Util.getBufferedreaderByFileName(Constants.ORACLE_FILE_NAME);
 		//Util.printFile(bufferedReader);
 		sentimentDataList= SentimentData.parseSentimentData(bufferedReader);
 		System.out.println(sentimentDataList.size());
-		bufferedReader = Util.getBufferedreaderByFileName(Constants.CONTRACTION_TEXT_FILE_NAME);
-		contractionMap = ContractionLoader.getContractionData(bufferedReader);
-		for (String key : contractionMap.keySet())
-			System.out.println(key);
+		sentimentDataList=ContractionLoader.preprocessContractions(sentimentDataList);
+		for(int i = 0;i < sentimentDataList.size();i++)
+		{
+			System.out.println(sentimentDataList.get(i).getText());
+		}
+		
 	}
 
 }
