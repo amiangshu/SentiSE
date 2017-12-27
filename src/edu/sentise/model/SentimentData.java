b7e56f62e7c1b4ba3141b9220514cc4384ebdb7a
tanzeerH <hossaintanzeer@gmail.com>
2017-12-27 01:33:09 -0500

    contraction parser added

---
+++ src/edu/sentise/model/SentimentData.java
@@ -0,0 +1,85 @@
+package edu.sentise.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import edu.sentise.util.Constants;
+
+public class SentimentData {
+
+	private String text;
+	private int rating;
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public SentimentData(String text, int rating) {
+	
+		this.text = text;
+		this.rating = rating;
+	}
+	public static ArrayList<SentimentData> parseSentimentData(BufferedReader bufferedReader)
+	{
+		ArrayList<SentimentData> sentimentDataList =  new ArrayList<>();
+		 try {
+
+	            FileInputStream excelFile = new FileInputStream(new File(Constants.ORACLE_FILE_NAME));
+	            Workbook workbook = new XSSFWorkbook(excelFile);
+	            Sheet datatypeSheet = workbook.getSheetAt(0);
+	            Iterator<Row> iterator = datatypeSheet.iterator();
+
+	            while (iterator.hasNext()) {
+
+	                Row currentRow = iterator.next();
+	                Iterator<Cell> cellIterator = currentRow.iterator();
+	                //SentimentData sentimentData=new SentimentData(text, rating)
+	                String text="";
+	                int rating=0;
+	                while (cellIterator.hasNext()) {
+
+	                    Cell currentCell = cellIterator.next();
+	                  
+	                    //getCellTypeEnum shown as deprecated for version 3.15
+	                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+	                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+	                    	text=currentCell.getStringCellValue();
+	                       // System.out.print(currentCell.getStringCellValue() + "--");
+	                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+	                       // System.out.print(currentCell.getNumericCellValue() + "--");
+	                    	rating=(int)currentCell.getNumericCellValue(); 
+	                    }
+
+	                }
+	                SentimentData sentimentData=new SentimentData(text, rating);
+	                sentimentDataList.add(sentimentData);
+	                //System.out.println();
+
+	            }
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		 return sentimentDataList;
+	}
+}
--- src/edu/sentise/util/Constants.java
+++ src/edu/sentise/util/Constants.java
@@ -2,8 +2,8 @@ package edu.sentise.util;
 
 public class Constants {
 
-	public static final String CONTRACTION_TEXT_FILE_NAME="Contractions.txt";
-	public static final String EMOTICONS_FILE_NAME="EmoticonLookupTable.txt";
+	public static final String CONTRACTION_TEXT_FILE_NAME="src/Contractions.txt";
+	public static final String EMOTICONS_FILE_NAME="src/EmoticonLookupTable.txt";
 	public static final String ORACLE_FILE_NAME="src/oracle.xlsx";
 
 }
