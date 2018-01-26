56f22679200e7efc6eca951954553836feb46144
Amiangshu Bosu <amiangshu.bosu@siu.edu>
2018-01-26 13:14:41 -0600

    Updated oracle

--- src/edu/sentise/util/Constants.java
+++ src/edu/sentise/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String CONTRACTION_TEXT_FILE_NAME="src/Contractions.txt";
 	public static final String EMOTICONS_FILE_NAME="src/EmoticonLookupTable.txt";
 	public static final String STOPWORDS_FILE_NAME="src/StopWords.txt";
-	public static final String ORACLE_FILE_NAME="src/combined.xlsx";
+	public static final String ORACLE_FILE_NAME="src/sentise-oracle.xlsx";
 	public static final String ARFF_ORACLE_FILE_NAME="src/myoracle.arff";
 	public static final String ARFF_ORACLE_FILE_NAME_TEST="src/myoracle_test.arff";
 	public static final String DELIMITERS=" \\r\\t\\n.,;:\\'\\\"()?!-><#$\\\\%&*+/@^=[]{}|`~0123456789\\'�ⴾ��₄㬸������";
