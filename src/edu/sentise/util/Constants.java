02d19cd7b3716cc2ee49cdd6c8d28b407ab52e67
Tanzeer <tanzeer@CS-BOSU-NODE-1.cs.siu.edu>
2018-04-10 18:08:29 -0500

    underscore changed to pipe

--- src/edu/sentise/util/Constants.java
+++ src/edu/sentise/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String ORACLE_FILE_NAME="models/copy_combined.xlsx";
 	public static final String ARFF_ORACLE_FILE_NAME="models/myoracle.arff";
 	public static final String ARFF_ORACLE_FILE_NAME_TEST="models/myoracle_test.arff";
-	public static final String DELIMITERS=" \\r\\t\\n.,;:\\'\\\"()?!-><#$\\\\%&*+/@^=[]{}|`~0123456789\\'�ⴾ��₄㬸������";
+	public static final String DELIMITERS=" \\r\\t\\n.,;:\\'\\\"()?!-><#$\\\\%&*+/@^=[]{}`~0123456789\\'�ⴾ��₄㬸������_";
 	public static final String MODEL_FILE_NAME="models/rf.combined.xlsx.model";
 	public static final String NEG_DATA_FILE_NAME="models/neg_data.txt"; 
     public static final String POSITIVE_SENTI_WORD_FILE="models/positive.txt"; 
