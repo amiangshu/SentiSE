c1bb2150d031b2b2e463dfc72970fd538627f31e
Tanzeer <tanzeer@CS-BOSU-NODE-1.cs.siu.edu>
2018-04-19 21:51:08 -0500

    local changes

--- automate_rf.py
+++ automate_rf.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import subprocess as sp
-arg_list=[['java','-jar','sentise.jar','-algo','RF','-negate'],['java','-jar','sentise.jar','-algo','RF','-ngram'],
-['java','-jar','sentise.jar','-algo','RF','-tag','1'],['java','-jar','sentise.jar','-algo','RF','-tag','2'],['java','-jar','sentise.jar','-algo','RF','-punctuation','1'],
-['java','-jar','sentise.jar','-algo','RF','-punctuation','2'],['java','-jar','sentise.jar','-algo','RF','-root','1'],
+arg_list=[['java','-jar','sentise.jar','-algo','RF','-punctuation','2'],['java','-jar','sentise.jar','-algo','RF','-root','1'],
 ['java','-jar','sentise.jar','-algo','RF','-root','2'],['java','-jar','sentise.jar','-algo','RF','-features','2'],
 ['java','-jar','sentise.jar','-algo','RF','-sentiword','2'],['java','-jar','sentise.jar','-algo','RF','-sentiword','4']]
 for arg in arg_list:
