498b860855983d221731deb493ba1e268e9c0753
Amiangshu Bosu <asbosu@ua.edu>
2018-05-11 00:45:50 -0500

    Removed debug statements from CNN jar

--- src/edu/sentise/Configuration.java
+++ src/edu/sentise/Configuration.java
@@ -2,28 +2,25 @@ package edu.sentise;
 
 public class Configuration {
 
-	public static final String CONTRACTION_TEXT_FILE_NAME="models/Contractions.txt";
-	public static final String EMOTICONS_FILE_NAME="models/EmoticonLookupTable.txt";
-	public static final String EMOTICONS_CATEGORIZED="models/EmoticonLookupCategorized.txt";
-	public static final String STOPWORDS_FILE_NAME="models/StopWords.txt";
-	public static final String ORACLE_FILE_NAME="models/sentise-oracle-short.xlsx";
-	public static final String ARFF_ORACLE_FILE_NAME="models/myoracle.arff";
-	public static final String ARFF_ORACLE_FILE_NAME_TEST="models/myoracle_test.arff";
-	public static final String DELIMITERS=" \\r\\t\\n.,;:\\'\\\"()?!-><#$\\\\%&*+/@^=[]{}`~0123456789\\'|";
-	public static final String MODEL_FILE_NAME="models/rf.combined.xlsx.model";
-	public static final String NEG_DATA_FILE_NAME="models/neg_data.txt"; 
-    public static final String POSITIVE_SENTI_WORD_FILE="models/positive.txt"; 
-    public static final String NEGATIVE_SENTI_WORD_FILE="models/negative.txt";
-    public static final String SWEAR_WORD_FILE="models/swear_words.txt";
-    public static final String BIGRAM_FILE="models/bigram.txt";
-    public static final String TRIGRAM_FILE="models/trigram.txt";
-    public static final String ACRONYM_WORD_FILE="models/short_word_list.txt"; 
-    public static final String KEYWORD_LIST_FILE="models/keyword_list.txt";
-    public static final String OUTPUT_DIRECTORY="output/"; 
-    public static final String ARFF_DIRECTORY="arff/";
-	public static final String EMPTY_FILE = "models/EmptyStopWords.txt"; 
-
-
-
+	public static final String CONTRACTION_TEXT_FILE_NAME = "models/Contractions.txt";
+	public static final String EMOTICONS_FILE_NAME = "models/EmoticonLookupTable.txt";
+	public static final String EMOTICONS_CATEGORIZED = "models/EmoticonLookupCategorized.txt";
+	public static final String STOPWORDS_FILE_NAME = "models/StopWords.txt";
+	public static final String ORACLE_FILE_NAME = "models/sentise-oracle-short.xlsx";
+	public static final String ARFF_ORACLE_FILE_NAME = "models/myoracle.arff";
+	public static final String ARFF_ORACLE_FILE_NAME_TEST = "models/myoracle_test.arff";
+	public static final String DELIMITERS = " \\r\\t\\n.,;:\\'\\\"()?!-><#$\\\\%&*+/@^=[]{}`~0123456789\\'|";
+	public static final String MODEL_FILE_NAME = "models/rf.combined.xlsx.model";
+	public static final String NEG_DATA_FILE_NAME = "models/neg_data.txt";
+	public static final String POSITIVE_SENTI_WORD_FILE = "models/positive.txt";
+	public static final String NEGATIVE_SENTI_WORD_FILE = "models/negative.txt";
+	public static final String SWEAR_WORD_FILE = "models/swear_words.txt";
+	public static final String BIGRAM_FILE = "models/bigram.txt";
+	public static final String TRIGRAM_FILE = "models/trigram.txt";
+	public static final String ACRONYM_WORD_FILE = "models/short_word_list.txt";
+	public static final String KEYWORD_LIST_FILE = "models/keyword_list.txt";
+	public static final String OUTPUT_DIRECTORY = "output/";
+	public static final String ARFF_DIRECTORY = "arff/";
+	public static final String EMPTY_FILE = "models/EmptyStopWords.txt";
 
 }
